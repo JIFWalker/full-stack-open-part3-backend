2272610f212ff81aa11a6a2999fb89e9abff81ff
JIFWalker <jiwww.walker@gmail.com>
2021-07-05 20:56:51 -0700

    part 3.8

--- index.js
+++ index.js
@@ -2,56 +2,58 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
-app.use(morgan('tiny'))
-
-
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
+morgan.token('data', function (req) {
+  return JSON.stringify(req.body)
+})
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.post('/api/persons', (request, response) => {
     const id = Math.floor(Math.random() * 1000000)
     const person = request.body
-
+    
     person.id = id
 
     switch (true) {
-        case persons.map(personList => personList.name).includes(person.name): 
-            console.log(`error: 'name must be unique'`)
-            response.status(409).end()
-            break
-        case person.name === "":
-           console.log(`error: 'name is missing!`)
-            response.status(406).end()
-            break
-        case person.number === "":
-          console.log(`error: 'number is missing!`)
+      case persons.map(personList => personList.name).includes(person.name): 
+          console.log(`error: 'name must be unique'`)
+          response.status(409).end()
+          break
+      case person.name === "":
+        console.log(`error: 'name is missing!`)
           response.status(406).end()
           break
-    default:
-            persons = persons.concat(person)
-            response.json(person)
+      case person.number === "":
+        console.log(`error: 'number is missing!`)
+        response.status(406).end()
+        break
+      default:
+        persons = persons.concat(person)
+        response.json(person)
     }
   })
 
