4f3c8e0cfa7c62aef1b1837881341b31241b917e
JIFWalker <jiwww.walker@gmail.com>
2021-07-06 14:16:55 -0700

    heroku initialization test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+app.use(cors())
+
 
 let persons = [
   { 
@@ -85,6 +88,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
