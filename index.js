5995c1ab084cdca2608f0c3936a26cb3c4ba3168
JIFWalker <jiwww.walker@gmail.com>
2021-07-06 15:29:14 -0700

    part 3.9

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-app.use(cors())
 
 
 let persons = [
@@ -28,10 +27,14 @@ let persons = [
   }
 ]
 
+
 morgan.token('data', function (req) {
   return JSON.stringify(req.body)
 })
 
+
+app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
