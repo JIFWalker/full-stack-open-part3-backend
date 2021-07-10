c8811da7607664d500248e1f3b46879adb03b277
JIFWalker <jiwww.walker@gmail.com>
2021-07-09 17:17:43 -0700

    3.14

--- index.js
+++ index.js
@@ -4,55 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
+const Contact = require('./models/Contact')
 morgan.token('data', function (req) {
   return JSON.stringify(req.body)
 })
-
-//Connection to MONGODB Atlas database
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-const contactSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
-})
-
-const Contact = mongoose.model('Contact', contactSchema)
-
- 
-//Hardcoded Backup Data
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
 // Middleware
 app.use(cors())
@@ -71,27 +26,17 @@ app.get('/api/persons', (request, response) => {
 // Adds person to database
 app.post('/api/persons', (request, response) => {
     const id = Math.floor(Math.random() * 1000000)
-    const person = request.body
-    
-    person.id = id
+    const body = request.body
 
-    switch (true) {
-      case persons.map(personList => personList.name).includes(person.name): 
-          console.log(`error: 'name must be unique'`)
-          response.status(409).end()
-          break
-      case person.name === "":
-        console.log(`error: 'name is missing!`)
-          response.status(406).end()
-          break
-      case person.number === "":
-        console.log(`error: 'number is missing!`)
-        response.status(406).end()
-        break
-      default:
-        persons = persons.concat(person)
-        response.json(person)
-    }
+    const person = new Contact({
+      _id: id,
+      name: body.name,
+      number: body.number
+    })
+
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
   })
 
 
