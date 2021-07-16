37b48ad5c11a16df8b6ca62b4e0d87e75857b96a
JIFWalker <jiwww.walker@gmail.com>
2021-07-16 15:11:10 -0700

    part 3.22

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 const Contact = require('./models/Contact')
 morgan.token('data', function (req) {
-  return JSON.stringify(req.body)
+    return JSON.stringify(req.body)
 })
 
 
@@ -17,9 +17,9 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 
 //Gets all contact data from database
 app.get('/api/persons', (request, response) => {
-  Contact.find({}).then(contacts => {
-    response.json(contacts)
-  })
+    Contact.find({}).then(contacts => {
+        response.json(contacts)
+    })
 })
 
 
@@ -28,84 +28,84 @@ app.post('/api/persons', (request, response, next) => {
     const id = Math.floor(Math.random() * 1000000)
     const body = request.body
     const person = new Contact({
-      _id: id,
-      name: body.name,
-      number: body.number
+        _id: id,
+        name: body.name,
+        number: body.number
     })
 
     person.save()
-      .then(savedPerson => {
-      response.json(savedPerson)
-    })
-    .catch(error => next(error))
-  })
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
+})
 
 
 // Updates number for specified contact
 app.put('/api/persons/:id', (request, response, next) => {
-  const { number } = request.body
+    const { number } = request.body
 
-  Contact.findByIdAndUpdate(request.params.id, { number }, {
-    runValidators: true,
-    context: 'query'
-  })
-    .then(updatedContact => {
-      response.json(updatedContact)
+    Contact.findByIdAndUpdate(request.params.id, { number }, {
+        runValidators: true,
+        context: 'query'
     })
-    .catch(error => next(error))
+        .then(updatedContact => {
+            response.json(updatedContact)
+        })
+        .catch(error => next(error))
 })
 
 
 // Displays number of contacts and the current local date
 app.get('/info', (request, response) => {
-  Contact.countDocuments(({}), function(err, results){
-    response.send(`
+    Contact.countDocuments(({}), function(err, results){
+        response.send(`
     <p>Phonebook has info for ${results} people</p>
     <p>${Date()}</p>`
-    )
-  })
+        )
+    })
 })
 
 
 // Gets a specific contact's data from their ID
-app.get('/api/persons/:id', (request,response) => {
-  Contact.findById(request.params.id)
-    .then(contact => {
-      if (contact) {
-        response.json(contact)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+app.get('/api/persons/:id', (request,response, next) => {
+    Contact.findById(request.params.id)
+        .then(contact => {
+            if (contact) {
+                response.json(contact)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 // Deletes a contact from the phonebook
 app.delete('/api/persons/:id', (request, response, next) => {
-  Contact.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+    Contact.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 // Error handling middleware
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint'})
+    response.status(404).send({ error: 'unknown endpoint'})
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+    console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id'})
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
-  }
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
 
-  next(error)
+    next(error)
 }
 
 app.use(errorHandler)
@@ -113,5 +113,5 @@ app.use(errorHandler)
 // Port configuration
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
--- models/Contact.js
+++ models/Contact.js
@@ -6,27 +6,27 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const contactSchema = new mongoose.Schema({
-  _id: String,
-  name: {
-    type: String,
-    minLength: 3,
-    required: true,
-    unique: true
-  },
-  number: {
-    type: String,
-    minLength: 8,
-    required: true,
-    unique: true
-  },
+    _id: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+        unique: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        required: true,
+        unique: true
+    },
 })
 
 contactSchema.plugin(uniqueValidator)
